08420a89f88a788be41ed29d3d96ac1367ec5d3e
meherdeept <meherdeept@gmail.com>
2021-03-10 00:41:30 +0530

    request permission

--- app/src/main/java/com/agora/samtan/agorabroadcast/MainActivity.java
+++ app/src/main/java/com/agora/samtan/agorabroadcast/MainActivity.java
@@ -1,12 +1,16 @@
 package com.agora.samtan.agorabroadcast;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.RadioButton;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import io.agora.rtc.Constants;
 
@@ -20,6 +24,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        int MY_PERMISSIONS_REQUEST_CAMERA = 0;
+        // Here, this is the current activity
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA, Manifest.permission.RECORD_AUDIO}, MY_PERMISSIONS_REQUEST_CAMERA);
+
+        }
     }
 
     public void onRadioButtonClicked(View view) {
